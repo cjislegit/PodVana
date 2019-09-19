89d99088975af2d798d7d302b740106b38ce3504
cjislegit <carlosjramirez11@gmail.com>
2019-09-19 12:57:55 -0700

    Wrote js to fetch podcast data from itunes

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Test');
+const apiData = {
+  url: 'https://itunes.apple.com/search?',
+  term: 'term=joe+rogan',
+  media: '&media=podcast'
+};
+
+const { url, term, media } = apiData;
+
+const apiURL = `${url}${term}${media}`;
+
+fetch(apiURL)
+  .then(data => data.json())
+  .then(podcast => console.log(podcast));
