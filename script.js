3a6daa57d0a1f33285de7ec6b7c1cc10a6126c10
cjislegit <carlosjramirez11@gmail.com>
2019-09-20 14:27:53 -0700

    wrote js to pull name and art of podcast and insert into html

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const apiData = {
   url: 'https://itunes.apple.com/search?',
   term: 'term=joe+rogan',
-  media: '&media=podcast'
+  media: '&media=podcast',
+  limit: '&limit=1'
 };
 
-const { url, term, media } = apiData;
+const { url, term, media, limit } = apiData;
 
-const apiURL = `${url}${term}${media}`;
+const apiURL = `${url}${term}${media}${limit}`;
 
 fetch(apiURL)
   .then(data => data.json())
-  .then(podcast => console.log(podcast));
+  .then(podcast => generateMainContentHTML(podcast));
+
+const generateMainContentHTML = podcast => {
+  console.log(podcast['results'][0]['artistName']);
+  console.log(podcast['results'][0]['artworkUrl100']);
+
+  const mainContentPodcast = `<div class="podCast">
+          <img src="${podcast['results'][0]['artworkUrl100']}" />
+          <p>${podcast['results'][0]['artistName']}</p>
+        </div>`;
+
+  const mainContainerDiv = document.querySelector('.mainContainer');
+
+  mainContainerDiv.innerHTML = mainContentPodcast;
+};
