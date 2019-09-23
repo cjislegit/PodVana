3e16d56c957ae02395a23cf9e5b6dde9feadaf43
cjislegit <carlosjramirez11@gmail.com>
2019-09-23 14:26:11 -0700

    podCast now gets added for each result

--- script.js
+++ script.js
@@ -15,18 +15,28 @@ fetch(apiURL)
 
 const generateMainContentHTML = podcast => {
   podcast['results'].forEach(element => {
-    console.log(element['artistName']);
-  });
-
-  const mainContentPodcast = document.createElement('div'); //Creates div
-  mainContentPodcast.setAttribute('class', 'podCast'); //Gives div class of podCast
-  mainContentPodcast.innerHTML = `<img src="${
-    //Creates the inside of the div
-    podcast['results'][0]['artworkUrl100']
-  }" />
+    const mainContentPodcast = document.createElement('div'); //Creates div
+    mainContentPodcast.setAttribute('class', 'podCast'); //Gives div class of podCast
+    mainContentPodcast.innerHTML = `<img src="${
+      //Creates the inside of the div
+      podcast['results'][0]['artworkUrl100']
+    }" />
           <p>${podcast['results'][0]['artistName']}</p>`;
 
-  const mainContainerDiv = document.querySelector('.mainContainer');
+    const mainContainerDiv = document.querySelector('.mainContainer');
 
-  mainContainerDiv.appendChild(mainContentPodcast);
+    mainContainerDiv.appendChild(mainContentPodcast);
+  });
+
+  // const mainContentPodcast = document.createElement('div'); //Creates div
+  // mainContentPodcast.setAttribute('class', 'podCast'); //Gives div class of podCast
+  // mainContentPodcast.innerHTML = `<img src="${
+  //   //Creates the inside of the div
+  //   podcast['results'][0]['artworkUrl100']
+  // }" />
+  //         <p>${podcast['results'][0]['artistName']}</p>`;
+
+  // const mainContainerDiv = document.querySelector('.mainContainer');
+
+  // mainContainerDiv.appendChild(mainContentPodcast);
 };
