d021c82ba967c2d36c4001148b3491be428b4f2c
cjislegit <carlosjramirez11@gmail.com>
2019-12-18 13:50:04 -0800

    SW is now working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //If web browers supports sw one will be registered
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/js/sw.js")
+    .register("sw.js")
     .then(reg => console.log("Service worker registered", reg))
     .catch(err => console.log("Service worker not refistered", err));
 }
