9efacae96136f0a9d838da58c4a98e075961cdc5
cjislegit <carlosjramirez11@gmail.com>
2019-12-18 14:23:30 -0800

    Removed redundant js code form podcastScript file

--- js/podcastScript.js
+++ js/podcastScript.js
@@ -88,27 +88,6 @@ const formatLength = d => {
   return hDisplay + mDisplay + sDisplay;
 };
 
-//Check if audio is playing or paused and changes it
-const playAndPause = () => {
-  const player = document.querySelector("#player");
-
-  if (player.paused) {
-    const icon = document.querySelector(".fa-play");
-    const status = document.querySelector(".nowPlayingStatus");
-
-    player.play();
-    icon.setAttribute("class", "fas fa-pause");
-    status.innerHTML = "Playing";
-  } else {
-    const icon = document.querySelector(".fa-pause");
-    const status = document.querySelector(".nowPlayingStatus");
-
-    player.pause();
-    icon.setAttribute("class", "fas fa-play");
-    status.innerHTML = "Paused";
-  }
-};
-
 //Updates sound file in audio tag and plays the file
 const playTrack = (soundFile, imgFile, trackName) => {
   const player = document.querySelector("#player");
@@ -129,44 +108,6 @@ const playTrack = (soundFile, imgFile, trackName) => {
   name.innerHTML = trackName;
   img.setAttribute("src", imgFile);
 };
-
-// Checks if a track was played if so it stores the seconds and saves it to local storage
-const playingTrack = () => {
-  const player = document.querySelector("#player");
-  const title = document.querySelector(".nowPlayingName").textContent;
-  const art = document.querySelector(".nowPlayingArt img").src;
-
-  if (player.currentTime) {
-    let playingTrackInfo = {
-      src: player.src,
-      title: title,
-      art: art,
-      currentTime: player.currentTime
-    };
-    localStorage.setItem("currentTime", JSON.stringify(playingTrackInfo));
-  }
-};
-
-//Sets curretTime to the info passed from previous page
-let currentTime = JSON.parse(localStorage.getItem("currentTime"));
-localStorage.removeItem("currentTime");
-
-//Checks if currentTime has info if so it updates the nowPlaying section
-if (currentTime) {
-  const nowPlayingArt = document.querySelector(".nowPlayingArt img");
-  const nowPlayingName = document.querySelector(".nowPlayingName");
-  const player = document.querySelector("#player");
-  const icon = document.querySelector(".fa-play");
-  const status = document.querySelector(".nowPlayingStatus");
-
-  nowPlayingArt.setAttribute("src", currentTime["art"]);
-  nowPlayingName.innerHTML = currentTime["title"];
-  player.setAttribute("src", currentTime["src"]);
-  player.currentTime = currentTime["currentTime"];
-  player.play();
-  icon.setAttribute("class", "fas fa-pause");
-  status.innerHTML = "Playing";
-}
 
 //Changes styling on subscribe button
 const sub = () => {
