930c9372a78af2e366e4831adb28b6eb65bd7009
cjislegit <carlosjramirez11@gmail.com>
2019-12-19 16:43:59 -0800

    Added firebase

---
+++ js/fb.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA3_wJMaJ7WXRj1_4FL_7esO5SGl2r3JGM",
+  authDomain: "podvana-55ea6.firebaseapp.com",
+  databaseURL: "https://podvana-55ea6.firebaseio.com",
+  projectId: "podvana-55ea6",
+  storageBucket: "podvana-55ea6.appspot.com",
+  messagingSenderId: "67042435862",
+  appId: "1:67042435862:web:0eb7e23f05465028472637",
+  measurementId: "G-DRZ9GJEKL7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const db = firebase.firestore();
