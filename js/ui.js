93e2f389f76997268727298f7b768b9e773ebe3c
Carlos Ramirez <carlosjramirez11@gmail.com>
2019-12-21 15:57:12 -0800

    Subscribed podcast can now be clicked on to get to podcast page

--- js/ui.js
+++ js/ui.js
@@ -1,22 +1,23 @@
 const createSubPodcast = (data, id) => {
   let podcastURL =
-    "https://listen-api.listennotes.com/api/v2/podcasts/" + data["podcastId"];
+    'https://listen-api.listennotes.com/api/v2/podcasts/' + data['podcastId'];
   const h = new Headers();
-  const subscribeMainContainer = document.querySelector(".mainContainer");
+  const subscribeMainContainer = document.querySelector('.mainContainer');
 
-  h.append("X-ListenAPI-Key", "776c9171dbbc4181aad650262761ceaa");
+  h.append('X-ListenAPI-Key', '776c9171dbbc4181aad650262761ceaa');
   const reqPodcasts = new Request(podcastURL, {
-    method: "GET",
+    method: 'GET',
     headers: h,
-    mode: "cors"
+    mode: 'cors'
   });
 
   fetch(reqPodcasts)
     .then(data => data.json())
     .then(podcast => {
-      let html = `<div class="podCast">
-          <img src="${podcast["image"]}" />
-          <p>${podcast["title"]}</p>
+      let html = `<div class="podCast" id="${podcast['id']}" onclick="saveIdtoLocalStorage(id)"><a href='/pages/podcast.html'>
+          <img src="${podcast['image']}" />
+          <p>${podcast['title']}</p>
+          </a>
         </div>`;
       subscribeMainContainer.innerHTML += html;
     });
