130da936f4c764562c85a04c1c252964851127c9
Carlos Ramirez <carlosjramirez11@gmail.com>
2019-12-21 16:44:17 -0800

    Podcast can now be subscribed to

--- js/db.js
+++ js/db.js
@@ -1,13 +1,22 @@
 //Real-time listener
-db.collection("subscriptions").onSnapshot(snapshot => {
+db.collection('subscriptions').onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
-    if (change.type == "added") {
+    if (change.type == 'added') {
       //Add podast to subsction page
       createSubPodcast(change.doc.data(), change.doc.id);
     }
 
-    if (change.type == "remove") {
+    if (change.type == 'remove') {
       //Remove podcast subsction page
     }
   });
 });
+
+//Add new subscription
+const addNewSub = podcastId => {
+  const newSub = {
+    podcastId: podcastId
+  };
+
+  db.collection('subscriptions').add(newSub);
+};
--- js/podcastScript.js
+++ js/podcastScript.js
@@ -1,32 +1,31 @@
-let id = localStorage["objectToPass"];
-localStorage.removeItem("objectToPass");
+let id = localStorage['objectToPass'];
+localStorage.removeItem('objectToPass');
 
-let podcastURL = "https://listen-api.listennotes.com/api/v2/podcasts/" + id;
+let podcastURL = 'https://listen-api.listennotes.com/api/v2/podcasts/' + id;
 
 const h = new Headers();
 
-h.append("X-ListenAPI-Key", "776c9171dbbc4181aad650262761ceaa");
+h.append('X-ListenAPI-Key', '776c9171dbbc4181aad650262761ceaa');
 
 const reqPodcasts = new Request(podcastURL, {
-  method: "GET",
+  method: 'GET',
   headers: h,
-  mode: "cors"
+  mode: 'cors'
 });
 fetch(reqPodcasts)
   .then(data => data.json())
   .then(podcast => generatePodcastImgHTML(podcast));
 
 const generatePodcastImgHTML = podcast => {
-  let img = podcast["image"];
-  let episodes = podcast["episodes"];
-  const podCastImg = document.createElement("img"); //Creates img
-  const podcastButtonsContainer = document.createElement("div");
-  const podcastArt = document.querySelector(".podcastArt"); //Gets the podcast img container
+  let img = podcast['image'];
+  let episodes = podcast['episodes'];
+  const podCastImg = document.createElement('img'); //Creates img
+  const podcastButtonsContainer = document.createElement('div');
+  const podcastArt = document.querySelector('.podcastArt'); //Gets the podcast img container
 
-  podCastImg.setAttribute("src", img); //Sets src to img url
-  podcastButtonsContainer.setAttribute("class", "podcastButtonsContainer");
-  podcastButtonsContainer.innerHTML =
-    '<span onclick="sub()">Subscribe</span> &nbsp <i class="fa fa-check-square notSubbed" aria-hidden="true"></i>';
+  podCastImg.setAttribute('src', img); //Sets src to img url
+  podcastButtonsContainer.setAttribute('class', 'podcastButtonsContainer');
+  podcastButtonsContainer.innerHTML = `<span onclick="addNewSub('${podcast['id']}')">Subscribe</span> &nbsp <i class="fa fa-check-square notSubbed" aria-hidden="true"></i>`;
   podcastArt.appendChild(podCastImg); //Adds img to the container
   podcastArt.appendChild(podcastButtonsContainer); //Adds buttons
 
@@ -35,36 +34,36 @@ const generatePodcastImgHTML = podcast => {
 
 const generatePodcastTracksHTML = podcast => {
   podcast.forEach(track => {
-    const podcastTracks = document.createElement("div");
-    const podcastTracksContainer = document.createElement("div");
-    const podcastTrackName = document.createElement("div");
-    const podcastTrackLength = document.createElement("div");
-    const podcastTrackDate = document.createElement("div");
-    const podcastTrackStatus = document.createElement("div");
-    const podcastTrackStatusIcon = document.createElement("i");
+    const podcastTracks = document.createElement('div');
+    const podcastTracksContainer = document.createElement('div');
+    const podcastTrackName = document.createElement('div');
+    const podcastTrackLength = document.createElement('div');
+    const podcastTrackDate = document.createElement('div');
+    const podcastTrackStatus = document.createElement('div');
+    const podcastTrackStatusIcon = document.createElement('i');
 
-    podcastTracks.setAttribute("class", "podcastTracks");
+    podcastTracks.setAttribute('class', 'podcastTracks');
     podcastTracks.setAttribute(
-      "onclick",
-      `playTrack('${track["audio"]}', '${track["image"]}', '${track["title"]}' )`
+      'onclick',
+      `playTrack('${track['audio']}', '${track['image']}', '${track['title']}' )`
     );
 
-    podcastTracksContainer.setAttribute("class", "podcastTracksContainer");
-    podcastTrackName.setAttribute("class", "podcastTrackName");
-    podcastTrackName.innerHTML = `<strong>${track["title"]}</strong>`;
+    podcastTracksContainer.setAttribute('class', 'podcastTracksContainer');
+    podcastTrackName.setAttribute('class', 'podcastTrackName');
+    podcastTrackName.innerHTML = `<strong>${track['title']}</strong>`;
 
-    podcastTrackLength.setAttribute("class", "podcastTrackLength");
-    podcastTrackLength.innerHTML = `${formatLength(track["audio_length_sec"])}`;
+    podcastTrackLength.setAttribute('class', 'podcastTrackLength');
+    podcastTrackLength.innerHTML = `${formatLength(track['audio_length_sec'])}`;
 
-    podcastTrackDate.setAttribute("class", "podcastTrackDate");
+    podcastTrackDate.setAttribute('class', 'podcastTrackDate');
     podcastTrackDate.innerHTML = `${new Date(
-      track["pub_date_ms"]
-    ).toLocaleDateString("en-US")}`;
+      track['pub_date_ms']
+    ).toLocaleDateString('en-US')}`;
 
-    podcastTrackStatus.setAttribute("class", "podcastTrackStatus");
+    podcastTrackStatus.setAttribute('class', 'podcastTrackStatus');
     podcastTrackStatus.innerHTML = `<i class="fas fa-align-left fa-rotate-270"></i>`;
 
-    let podcastContainer = document.querySelector(".podcastContainer");
+    let podcastContainer = document.querySelector('.podcastContainer');
 
     podcastTracksContainer.appendChild(podcastTrackName);
     podcastTracksContainer.appendChild(podcastTrackLength);
@@ -82,37 +81,37 @@ const formatLength = d => {
   var m = Math.floor((d % 3600) / 60);
   var s = Math.floor((d % 3600) % 60);
 
-  var hDisplay = h > 0 ? h + ":" : "";
-  var mDisplay = m > 0 ? m + ":" : "";
-  var sDisplay = s > 0 ? s : "";
+  var hDisplay = h > 0 ? h + ':' : '';
+  var mDisplay = m > 0 ? m + ':' : '';
+  var sDisplay = s > 0 ? s : '';
   return hDisplay + mDisplay + sDisplay;
 };
 
 //Updates sound file in audio tag and plays the file
 const playTrack = (soundFile, imgFile, trackName) => {
-  const player = document.querySelector("#player");
-  const icon = document.querySelector(".fa-play");
-  const status = document.querySelector(".nowPlayingStatus");
-  let name = document.querySelector(".nowPlayingName");
-  let img = document.querySelector(".nowPlayingArt img");
+  const player = document.querySelector('#player');
+  const icon = document.querySelector('.fa-play');
+  const status = document.querySelector('.nowPlayingStatus');
+  let name = document.querySelector('.nowPlayingName');
+  let img = document.querySelector('.nowPlayingArt img');
 
-  player.setAttribute("src", soundFile);
+  player.setAttribute('src', soundFile);
   player.play();
 
   if (icon !== null) {
     //Checks status of icon and skips if already set to pause
-    icon.setAttribute("class", "fas fa-pause");
+    icon.setAttribute('class', 'fas fa-pause');
   }
 
-  status.innerHTML = "Playing";
+  status.innerHTML = 'Playing';
   name.innerHTML = trackName;
-  img.setAttribute("src", imgFile);
+  img.setAttribute('src', imgFile);
 };
 
 //Changes styling on subscribe button
 const sub = () => {
-  let subStatus = document.querySelector(".notSubbed");
+  let subStatus = document.querySelector('.notSubbed');
   if (subStatus !== null) {
-    subStatus.setAttribute("class", "fa fa-check-square subbed");
+    subStatus.setAttribute('class', 'fa fa-check-square subbed');
   }
 };
