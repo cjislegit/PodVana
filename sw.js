630df41c9ff4487b3b0294a00e433799f3bc7ae1
Carlos Ramirez <carlosjramirez11@gmail.com>
2019-12-31 17:58:20 -0800

    Offline message has been styled

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
-const staticCacheName = "site-static-test";
+const staticCacheName = 'site-static-test';
 const assetes = [
-  "/",
-  "/index.html",
-  "/js/script.js",
-  "/pages/podcast.html",
-  "/js/podcastScript.js",
-  "/pages/search.html",
-  "/js/searchScript.js",
-  "/pages/subscribed.html",
-  "/js/subscribedScript.js",
-  "/styles/styles.css",
-  "/img/PF-liveops_InvariantCulture_Default.jpg",
-  "https://fonts.googleapis.com/css?family=Lato:100,900&display=swap",
-  "/pages/fallback.html",
-  "/js/fb.js",
-  "js/db.js",
-  "js/searchAndContinuity.js",
-  "js/ui.js",
-  "./keys.js",
-  "./manifest.json"
+  '/',
+  '/index.html',
+  '/js/script.js',
+  '/pages/podcast.html',
+  '/js/podcastScript.js',
+  '/pages/search.html',
+  '/js/searchScript.js',
+  '/pages/subscribed.html',
+  '/js/subscribedScript.js',
+  '/styles/styles.css',
+  '/img/PF-liveops_InvariantCulture_Default.jpg',
+  'https://fonts.googleapis.com/css?family=Lato:100,900&display=swap',
+  '/pages/fallback.html',
+  '/js/fb.js',
+  'js/db.js',
+  'js/searchAndContinuity.js',
+  'js/ui.js',
+  './keys.js',
+  './manifest.json'
 ];
 
 //Install service worker
-self.addEventListener("install", evt => {
+self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(staticCacheName).then(cache => {
       cache.addAll(assetes);
@@ -31,7 +31,7 @@ self.addEventListener("install", evt => {
 });
 
 //Listening for activating of service worker
-self.addEventListener("activate", evt => {
+self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
@@ -44,7 +44,7 @@ self.addEventListener("activate", evt => {
 });
 
 // Fetch event
-self.addEventListener("fetch", evt => {
+self.addEventListener('fetch', evt => {
   //Checks if requested file is in cache and returns it if it is
   evt.respondWith(
     caches.match(evt.request).then(cachesRes => {
